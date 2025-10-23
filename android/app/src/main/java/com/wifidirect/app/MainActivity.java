a8919c329c8f20b19f111c26daa7c350a68006d9
Karen Alvarado <53838303+karenHuff@users.noreply.github.com>
2025-10-23 11:52:05 -0600

    [UPDATE]
    Removed the onCreate method and WifiDirectPlugin registration.

--- android/app/src/main/java/com/wifidirect/app/MainActivity.java
+++ android/app/src/main/java/com/wifidirect/app/MainActivity.java
@@ -3,13 +3,5 @@ package com.wifidirect.app;
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-import com.wifidirect.plugins.WifiDirectPlugin;
-
-public class MainActivity extends BridgeActivity {
-   @Override
-   public void onCreate(Bundle savedInstanceState) {
-      registerPlugin(WifiDirectPlugin.class);
-      super.onCreate(savedInstanceState);
-   }
-}
+public class MainActivity extends BridgeActivity {}
 
