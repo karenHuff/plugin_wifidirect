f01a8690a7f3e9a7d7c6601ecf1f9ff885759bba
Karen Alvarado <53838303+karenHuff@users.noreply.github.com>
2026-08-27 13:40:25 -0600

    [UPDATE] delete file

--- src/capacitor-wifiDirect/definitions.ts
+++
@@ -1,7 +0,0 @@
-export interface WifiDirectPlugin {
-  startDiscovery(): Promise<{ value: string }>;
-  startTransfer(options: { file: string, device: string}): Promise<{ value: string }>;
-  startServerSocket(): Promise<{ value: string }>;
-  connectTo(options: { deviceAddress: string }): Promise<{ value: string }>;
-  closeConnection(): Promise<{ value: string }>;
-}
