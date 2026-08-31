1fdbfe0db16ffbb4721cd23e142f43b7c8fbe443
Karen Alvarado <53838303+karenHuff@users.noreply.github.com>
2026-08-31 14:25:38 -0600

    [UPDATE] solicitud de permisos con capacitor

--- android/app/src/main/java/com/example/plugins/WifiDirectReceiver.java
+++ android/app/src/main/java/com/example/plugins/WifiDirectReceiver.java
@@ -1,19 +1,12 @@
 package com.example.plugins;
 
-import android.Manifest;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.NetworkInfo;
 import android.net.wifi.p2p.WifiP2pDevice;
 import android.net.wifi.p2p.WifiP2pManager;
-import android.os.Build;
 import android.util.Log;
-
-import androidx.core.app.ActivityCompat;
-
-import com.example.sockets.StartServerSocket;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,8 +16,6 @@ public class WifiDirectReceiver extends BroadcastReceiver {
     private final WifiP2pManager manager;
     private final WifiP2pManager.Channel channel;
     private final WifiDirectPlugin plugin;
-    private final List<WifiP2pDevice> peers = new ArrayList<>();
-    private boolean serverStart = false;
 
     public WifiDirectReceiver(WifiP2pManager manager, WifiP2pManager.Channel channel, WifiDirectPlugin plugin) {
         this.manager = manager;
@@ -33,10 +24,10 @@ public class WifiDirectReceiver extends BroadcastReceiver {
     }
 
     @Override
+    @SuppressWarnings("MissingPermission")
     public void onReceive(Context context, Intent intent) {
         String action = intent.getAction();
-        if (action == null)
-            return;
+        if (action == null) return;
 
         switch (action) {
             case WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION:
@@ -48,19 +39,10 @@ public class WifiDirectReceiver extends BroadcastReceiver {
 
             case WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION:
                 if (manager != null) {
-                    if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(context, Manifest.permission.NEARBY_WIFI_DEVICES) != PackageManager.PERMISSION_GRANTED) {
-                        return;
-                    }
                     manager.requestPeers(channel, peerList -> {
-                        peers.clear();
-                        peers.addAll(peerList.getDeviceList());
-
-                        if (peers.isEmpty()) {
-                            Log.d("peers", "No se encontraron dispositivos");
-                        }
-
+                        List<WifiP2pDevice> devices = new ArrayList<>(peerList.getDeviceList());
                         // Enviar lista de peers al plugin
-                        plugin.onListPeers(peers);
+                        plugin.onListPeers(devices);
                     });
                 }
                 break;
@@ -69,29 +51,22 @@ public class WifiDirectReceiver extends BroadcastReceiver {
                 NetworkInfo networkInfo = intent.getParcelableExtra(WifiP2pManager.EXTRA_NETWORK_INFO);
 
                 if (networkInfo != null && networkInfo.isConnected()) {
-                    Log.d(TAG, "Interfaz P2P asociada, solicitando detalles del grupo...");
-
                     // Consultar el estado del grupo P2P
                     manager.requestConnectionInfo(channel, info -> {
                         if (info != null && info.groupFormed) {
                             if (info.isGroupOwner) {
                                 Log.d(TAG, "Soy el Group Owner");
-                                if (!serverStart) {
-                                    serverStart = true;
-                                    new StartServerSocket(context, plugin).start();
-                                }
+                                // iniciar servidor
                             } else {
                                 Log.d(TAG, "Preparando cliente");
-                                plugin.onResSocket(true);
+                                plugin.onClientStarted();
                             }
                         } else {
                             Log.d(TAG, "El grupo P2P aún no está completamente formado. Esperando...");
                         }
                     });
-
                 } else {
                     Log.d(TAG, "Desconectado del grupo P2P");
-                    serverStart = false;
                     plugin.onDisconnected();
                 }
                 break;
